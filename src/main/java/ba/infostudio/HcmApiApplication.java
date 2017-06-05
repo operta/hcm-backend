2dd545310c695b69442f4faf17e704c72339b840
tajib <tajib@Tajib-T470p>
2017-06-05 11:18:00 +0200

    Izmjena test

--- src/main/java/ba/infostudio/HcmApiApplication.java
+++ src/main/java/ba/infostudio/HcmApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HcmApiApplication {
 
-	//nesto
 	public static void main(String[] args) {
 		SpringApplication.run(HcmApiApplication.class, args);
 	}
