ba728c07e3dc70d4a47541d65a8ba4e4cefb4d58
Dino Begoli <dino.begoli@infostudio.ba>
2018-01-08 10:09:58 +0100

    Pagination

--- src/main/java/ba/infostudio/hcm/atVacancies/AtVacancyController.java
+++ src/main/java/ba/infostudio/hcm/atVacancies/AtVacancyController.java
@@ -1,10 +1,12 @@
 package ba.infostudio.hcm.atVacancies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/vacancies")
@@ -13,21 +15,30 @@ public class AtVacancyController {
     @Autowired
     private AtVacancyService atVacancyService;
 
-    @GetMapping("")
-    public @ResponseBody Iterable<AtVacancyModel> getAllVacancies(){
+/*    @GetMapping("")
+    public @ResponseBody
+    Page<AtVacancyModel> getAllVacancies(){
         return this.atVacancyService.getAllVacancies();
-    }
-
-
-    @GetMapping("/active")
-    public @ResponseBody Iterable<AtVacancyModel> getActiveVacancies() {
-        return this.atVacancyService.getActiveVacancies();
-    }
-
+    }*/
     @GetMapping("/{id}")
     public @ResponseBody AtVacancyModel getVacancy(@PathVariable String id) {
         return this.atVacancyService.getVacancy(Long.valueOf(id));
     }
+
+
+    @GetMapping("/{page}/{size}")
+    public @ResponseBody
+    Page<AtVacancyModel> getAllVacancies(@PathVariable String page, @PathVariable String size){
+        return this.atVacancyService.getAllVacancies(Integer.valueOf(page), Integer.valueOf(size));
+    }
+
+
+    @GetMapping("/active/{page}/{size}")
+    public @ResponseBody Page<AtVacancyModel> getActiveVacancies(@PathVariable String page, @PathVariable String size) {
+        return this.atVacancyService.getActiveVacancies(Integer.valueOf(page), Integer.valueOf(size));
+    }
+
+
 
 
 
